b3ab3c3290e3e4c5363d5b4db70abdad25d898ac
Durdona A <holisihlos@gmail.com>
2020-06-16 22:11:43 -0400

    Added TodoForm

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
 
 class TodoForm extends Component {
+	//creating  a controlled input with state
+	constructor (props) {
+		super(props);
+		this.state = { task: "" };
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	//reminds me Vue.JS two way data binding mode
+	handleChange (evt) {
+		this.setState({
+			[evt.target.name]: evt.target.value
+		});
+	}
+	handleSubmit (evt) {
+		evt.preventDefault(); // parent will pass a function as a props now
+		this.props.createTodo(this.state);
+		this.setState({ task: "" });
+	}
 	render () {
 		return (
-			<div>
-				<h2>Todo Form</h2>
-			</div>
+			<form onSubmit={this.handleSubmit}>
+				<label htmlFor="task">New Todo</label>
+				<input
+					type="text"
+					placeholder="New Todo"
+					name="task"
+					value={this.state.task}
+					onChange={this.handleChange}
+				/>
+				<button>Add Todo</button>
+			</form>
 		);
 	}
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
 
 class TodoList extends Component {
 	constructor (props) {
 		super(props);
-		this.state = { todos: [ { task: "Stay Focused" }, { task: "Prioritize Goal" }, { task: "Keep Working" } ] };
+		this.state = { todos: [ { task: "Stay Focused" }, { task: "Prioritize Goal" }, { task: "Keep Practicing" } ] };
+		this.create = this.create.bind(this);
+	}
+
+	create (newTodo) {
+		this.setState({
+			todos: [ ...this.state.todos, newTodo ] // making a copy of existing todos and adding a newTodo
+		});
 	}
 
 	render () {
@@ -13,6 +21,8 @@ class TodoList extends Component {
 		});
 		return (
 			<div>
+				<h1> Todo List</h1>
+				<TodoForm createTodo={this.create} />
 				<ul>{todos}</ul>
 			</div>
 		);
